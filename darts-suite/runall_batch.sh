218af5adf32686d4792679406cae8a468351e061
helenhe40 <helenhe40@users.noreply.github.com>
2023-06-22 15:27:59 -0700

    Update runall_batch.sh for Perlmutter CPU

--- darts-suite/runall_batch.sh
+++ darts-suite/runall_batch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH -q regular
 #SBATCH -N 1
-#SBATCH -C knl
+#SBATCH -C cpu
 #SBATCH -t 00:30:00
-#SBATCH --reservation=res_name
-#SBATCH -A ntrainN
+#SBATCH --reservation=crash_course     # this line only during 6/22 class 
+#SBATCH -A ntrain5         # or -A ntrain3 if you have an account from 6/8 class
+		      # use -A mxxxx after 6/22 class if you have a regular account
 
 # suggest to run part of the exercises from runall.sh at a time
 ./runall.sh

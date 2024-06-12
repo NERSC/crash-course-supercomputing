83b5d95c11c3c6ce5ab1c5a23cfcd8ea76860465
Erik <epalmer@lbl.gov>
2024-06-12 16:39:47 -0700

    Update darts.c move variable def inside for loop

--- darts-suite/c/darts.c
+++ darts-suite/c/darts.c
@@ -4,13 +4,12 @@
 static long num_trials = 1000000;
 
 int main(int argc, char **argv) {
-  long i;
   long Ncirc = 0;
   double pi, x, y;
   double r = 1.0; // radius of circle
   double r2 = r*r;
 
-  for (i = 0; i < num_trials; i++) {
+  for (long i = 0; i < num_trials; i++) {
     x = lcgrandom();
     y = lcgrandom();
     if ((x*x + y*y) <= r2)

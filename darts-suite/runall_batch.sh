c46b75220c349ee1e70cd1d6b502fd557c8aade5
helenhe40 <helenhe40@users.noreply.github.com>
2024-06-24 15:02:53 -0700

    Update runall_batch.sh

--- darts-suite/runall_batch.sh
+++ darts-suite/runall_batch.sh
@@ -3,9 +3,8 @@
 #SBATCH -N 1
 #SBATCH -C cpu
 #SBATCH -t 00:30:00
-#SBATCH --reservation=crash_course     # this line only during 6/22 class 
-#SBATCH -A ntrain5         # or -A ntrain3 if you have an account from 6/8 class
-		      # use -A mxxxx after 6/22 class if you have a regular account
+#SBATCH --reservation=crash_course     # this line only during 6/28 class 
+#SBATCH -A ntrain3     # use -A mxxxx after 6/28 class if you have a regular account
 
 # suggest to run part of the exercises from runall.sh at a time
 ./runall.sh

bbf13a0384c39097923185958025f8ce40384791
helenhe40 <helenhe40@users.noreply.github.com>
2025-06-04 15:32:15 -0700

    Update runall_batch.sh with 2025 project and date
    Update runall_batch.sh with 2025 project trn012, and remove specific mentioning of the training date

--- darts-suite/runall_batch.sh
+++ darts-suite/runall_batch.sh
@@ -3,8 +3,8 @@
 #SBATCH -N 1
 #SBATCH -C cpu
 #SBATCH -t 00:30:00
-#SBATCH --reservation=crash_course     # this line only during 6/28 class 
-#SBATCH -A ntrain3     # use -A mxxxx after 6/28 class if you have a regular account
+#SBATCH --reservation=crash_course     # this line only during the live class 
+#SBATCH -A trn012    # use -A mxxxx after class if you have a regular account
 
 # suggest to run part of the exercises from runall.sh at a time
 ./runall.sh

600e624b1b36fc499270ec849596e5bb54a43dc3
helenhe40 <helenhe40@users.noreply.github.com>
2026-06-23 18:37:04 -0700

    update code using thread-safe random number generator
    Refactor MPI code to use mpi_f08 and improve variable declarations.

--- darts-suite/fortran/darts-collective.f90
+++ darts-suite/fortran/darts-collective.f90
@@ -1,45 +1,47 @@
 ! Compute pi using MPI collectives
 program darts
-   use lcgenerator
-   use mpi
+    use lcgenerator
+    use mpi_f08
+    implicit none
+    
+    integer(kind=8)   :: num_trials = 1000000_8
+    integer(kind=8)   :: local_ncirc = 0_8
+    integer(kind=8)   :: total_ncirc = 0_8
+    integer(kind=8)   :: i, thread_seed, base_seed = 12345_8, my_trials
+    real(kind=8)      :: pi, x, y
+    real(kind=8)      :: r = 1.0_8
+    real(kind=8)      :: r2
+    integer           :: rank, size, manager = 0
+    integer           :: ierr
 
-   implicit none
-   integer :: num_trials = 1000000, i = 0, Ncirc = 0
-   real :: pi = 0.0, x = 0.0, y = 0.0, r = 1.0
-   real :: r2 = 0.0
+    call MPI_Init(ierr)
+    call MPI_Comm_size(MPI_COMM_WORLD, size, ierr)
+    call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
 
-   integer :: rank, np, manager = 0
-   integer :: mpierr
-   integer :: my_trials, temp
+    r2 = r * r
+    
+    my_trials = num_trials / int(size, 8)
 
-   call MPI_Init(mpierr)
-   call MPI_Comm_size(MPI_COMM_WORLD, np, mpierr)
-   call MPI_Comm_rank(MPI_COMM_WORLD, rank, mpierr)
-   r2 = r*r
-   my_trials = num_trials/np
-   if (mod(num_trials, np) .gt. rank) then
-      my_trials = my_trials+1
-   end if
-   call seed(rank)
+    do i = 1, my_trials
+        thread_seed = base_seed + (int(rank, 8) + ((i-1) * int(size, 8))) * 2_8
+        
+        call lcgrandom_r(thread_seed,x)
+        call lcgrandom_r(thread_seed,y)
+        
+        if ((x*x + y*y) <= r2) then
+            local_ncirc = local_ncirc + 1_8
+        end if
+    end do
 
-   do i = 1, my_trials
-      x = lcgrandom()
-      y = lcgrandom()
-      if ((x*x + y*y) .le. r2) then
-         Ncirc = Ncirc+1
-      end if
-   end do
+    call MPI_Reduce(local_ncirc, total_ncirc, 1, MPI_INTEGER8, MPI_SUM, manager, MPI_COMM_WORLD, ierr)
 
-   call MPI_Reduce(Ncirc, temp, 1, MPI_INTEGER, MPI_SUM, & 
-                   manager, MPI_COMM_WORLD, mpierr)
+    if (rank == manager) then
+        pi = 4.0_8 * real(total_ncirc, 8) / real(num_trials, 8)
+        print*, '     '
+        print*, '     Computing pi using six basic MPI functions:'
+        print '(/,A,I0,A,F10.6,/)', '      For ', num_trials, ' trials, pi = ', pi
+        print*, '     '
+    end if
 
-  if (rank .eq. manager) then
-     Ncirc = temp
-     pi = 4.0*((1.0*Ncirc)/(1.0*num_trials))
-     print*, '     '
-     print*, '     Computing pi using MPI collectives:'
-     print*, '     For ', num_trials, ' trials, pi = ', pi
-     print*, '     '
- end if
-     call MPI_Finalize(mpierr)
-end program
+    call MPI_Finalize(ierr)
+end program darts

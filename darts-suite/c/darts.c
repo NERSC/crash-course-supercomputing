677bd6c4288c38795e1e24af087706182066fb1e
helenhe40 <helenhe40@users.noreply.github.com>
2026-06-23 18:25:44 -0700

    using thread-safe random number generator
    Refactor main function to include thread-safe random number generation.

--- darts-suite/c/darts.c
+++ darts-suite/c/darts.c
@@ -1,26 +1,32 @@
 /* Compute pi in serial */
 #include "lcgenerator.h"
 #include <stdio.h>
-static long num_trials = 1000000;
 
-int main(int argc, char **argv) {
-  long Ncirc = 0;
-  double pi, x, y;
-  double r = 1.0; // radius of circle
-  double r2 = r*r;
+int main() {
+    long num_trials = 1000000;
+    long Ncirc = 0;
+    double pi, x, y;
+    double r = 1.0;
+    double r2 = r*r;
+    long base_seed = 12345;
+    long i;
 
-  for (long i = 0; i < num_trials; i++) {
-    x = lcgrandom();
-    y = lcgrandom();
-    if ((x*x + y*y) <= r2)
-      Ncirc++;
-  }
+    for (i = 0; i < num_trials; i++) {
+        long thread_seed = base_seed + (i * 2L);
 
-  pi = 4.0 * ((double)Ncirc)/((double)num_trials);
-  
-  printf("\n \t Computing pi in serial: \n");
-  printf("\t For %ld trials, pi = %f\n", num_trials, pi);
-  printf("\n");
+        x = lcgrandom_r(&thread_seed);
+        y = lcgrandom_r(&thread_seed);
 
-  return 0;
+        if ((x*x + y*y) <= r2) {
+            Ncirc++;
+        }
+    }
+
+    pi = 4.0 * ((double)Ncirc) / ((double)num_trials);
+
+    printf("\n \t Computing pi in serial: \n");
+    printf("\t For %ld trials, pi = %f\n", num_trials, pi);
+    printf("\n");
+
+    return 0;
 }

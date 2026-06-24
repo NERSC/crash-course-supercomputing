7065efc39ee59eac336c616c02a855ff92eb50e8
helenhe40 <helenhe40@users.noreply.github.com>
2026-06-24 15:48:43 -0700

    update darts-omp.f90 using !omp parallel do reduction

--- darts-suite/fortran/darts-omp.f90
+++ darts-suite/fortran/darts-omp.f90
@@ -1,4 +1,4 @@
-! Compute pi using OpenMP
+! Compute pi using OpenMP 
 program darts
     use lcgenerator
     use omp_lib
@@ -6,27 +6,17 @@ program darts
     
     integer(kind=8) :: num_trials = 1000000_8
     integer(kind=8) :: ncirc = 0_8
-    integer(kind=8) :: base_seed = 12345_8
-    real(kind=8)    :: pi
+    integer(kind=8) :: i, thread_seed, base_seed = 12345_8
+    real(kind=8)    :: pi, x, y
     real(kind=8)    :: r = 1.0_8
     real(kind=8)    :: r2
 
-    ! Hoisted thread-local variables
-    integer(kind=8) :: i, global_i, thread_seed, my_trials
-    real(kind=8)    :: x, y
-    integer         :: tid, total_threads
-
     r2 = r * r
 
-    !$omp parallel private(i, global_i, thread_seed, my_trials, x, y, tid, total_threads) reduction(+:ncirc)
-    
-    tid = omp_get_thread_num()
-    total_threads = omp_get_num_threads()
-    my_trials = num_trials / int(total_threads, 8)
-
-    do i = 1, my_trials
-        global_i = int(tid, 8) + ((i-1) * int(total_threads, 8))
-        thread_seed = base_seed + (global_i * 2_8)
+    !$omp parallel do private(thread_seed, x, y) reduction(+:ncirc)
+    do i = 0, num_trials - 1
+        ! Each iteration calculates its unique seed based on the loop index
+        thread_seed = base_seed + (i * 2_8)
         
         call lcgrandom_r(thread_seed, x)
         call lcgrandom_r(thread_seed, y)
@@ -35,12 +25,11 @@ program darts
             ncirc = ncirc + 1_8
         end if
     end do
-    
-    !$omp end parallel
+    !$omp end parallel do
 
     pi = 4.0_8 * real(ncirc, 8) / real(num_trials, 8)
     print*, '     '
     print*, '     Computing pi using OpenMP:'
     print '(/,A,I0,A,F10.6,/)', '      For ', num_trials, ' trials, pi = ', pi
-    print*, '     '
-end program darts
+    print*, '     '     
+end program

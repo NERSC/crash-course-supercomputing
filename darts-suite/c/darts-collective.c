dd98fed414febf57c20dd9c5d59de70b43021b3b
helenhe40 <helenhe40@users.noreply.github.com>
2026-06-23 18:29:29 -0700

    update code using thread-safe random number generator

--- darts-suite/c/darts-collective.c
+++ darts-suite/c/darts-collective.c
@@ -1,44 +1,44 @@
-/* Compute pi using MPI collectives */
+/* Compute pi using MPI Collectives */ 
 #include "lcgenerator.h"
 #include <mpi.h>
 #include <stdio.h>
 
-static long num_trials = 1000000;
-
 int main(int argc, char **argv) {
-  long i;
-  long Ncirc = 0;
-  double pi, x, y;
-  double r = 1.0; /* radius of circle */
-  double r2 = r*r;
+    long num_trials = 1000000;
+    long local_Ncirc = 0;
+    long total_Ncirc = 0;
+    double pi, x, y;
+    double r = 1.0;
+    double r2 = r*r;
+    long base_seed = 12345;
+    int rank, size, manager = 0;
 
-  int rank, size, manager = 0;
-  MPI_Status status;
-  long my_trials, temp;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  my_trials = num_trials/size;
-  if (num_trials%(long)size > (long)rank) my_trials++;
-  random_last = rank;
+    long my_trials = num_trials / size;
 
-  for (i = 0; i < my_trials; i++) {
-    x = lcgrandom();
-    y = lcgrandom();
-    if ((x*x + y*y) <= r2)
-      Ncirc++;
-  }
+    for (long i = 0; i < my_trials; i++) {
+        long thread_seed = base_seed + (rank + (i * size)) * 2L;
 
-  MPI_Reduce(&Ncirc, &temp, 1, MPI_LONG, MPI_SUM, manager, MPI_COMM_WORLD);
+        x = lcgrandom_r(&thread_seed);
+        y = lcgrandom_r(&thread_seed);
 
-  if (rank == manager) {
-    Ncirc = temp;
-    pi = 4.0 * ((double)Ncirc)/((double)num_trials);
-    printf("\n \t Computing pi using MPI collectives: \n");
-    printf("\t For %ld trials, pi = %f\n", num_trials, pi);
-    printf("\n");
-  } 
-  MPI_Finalize();
-  return 0;
+        if ((x*x + y*y) <= r2) {
+            local_Ncirc++;
+        }
+    }
+
+    MPI_Reduce(&local_Ncirc, &total_Ncirc, 1, MPI_LONG, MPI_SUM, manager, MPI_COMM_WORLD);
+
+    if (rank == manager) {
+        pi = 4.0 * ((double)total_Ncirc) / ((double)num_trials);
+        printf("\n \t Computing pi using MPI collectives: \n");
+        printf("\t For %ld trials, pi = %f\n", num_trials, pi);
+        printf("\n");
+    }
+
+    MPI_Finalize();
+    return 0;
 }

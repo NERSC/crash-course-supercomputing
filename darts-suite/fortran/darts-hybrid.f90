57747d931b9781bb593194de6e65729ad009d799
helenhe40 <helenhe40@users.noreply.github.com>
2026-06-23 18:38:14 -0700

    update code using thread-safe random number generator

--- darts-suite/fortran/darts-hybrid.f90
+++ darts-suite/fortran/darts-hybrid.f90
@@ -1,50 +1,70 @@
-! Compute pi hybrid MPI/OpenMP
+! Compute pi using hybrid MPI/OpenMP
 program darts
-   use lcgenerator
-   use mpi
+    use lcgenerator
+    use mpi_f08
+    use omp_lib
+    implicit none
+    
+    integer(kind=8)   :: num_trials = 1000000_8
+    integer(kind=8)   :: total_ncirc = 0_8
+    integer(kind=8)   :: base_seed = 12345_8
+    integer(kind=8)   :: trials_per_rank, trials_per_thread, local_rank_ncirc = 0_8
+    real(kind=8)      :: pi
+    real(kind=8)      :: r = 1.0_8
+    real(kind=8)      :: r2
+    integer           :: rank, size, manager = 0
+    integer           :: total_threads = 1
+    integer           :: provided, ierr
 
-   implicit none
-   integer :: num_trials = 1000000, i = 0, Ncirc = 0
-   real :: pi = 0.0, x = 0.0, y = 0.0, r = 1.0
-   real :: r2 = 0.0
+    integer(kind=8)   :: i, global_i, thread_seed
+    real(kind=8)      :: x, y
+    integer           :: tid
 
-   integer :: rank, np, manager = 0
-   integer :: provided, mpierr
-   integer :: my_trials, temp
+    call MPI_Init_thread(MPI_THREAD_FUNNELED, provided, ierr)
+    call MPI_Comm_size(MPI_COMM_WORLD, size, ierr)
+    call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
 
-   call MPI_Init_thread(MPI_THREAD_MULTIPLE, provided, mpierr)
-   call MPI_Comm_size(MPI_COMM_WORLD, np, mpierr)
-   call MPI_Comm_rank(MPI_COMM_WORLD, rank, mpierr)
-   r2 = r*r
-   my_trials = num_trials/np
-  if (mod(num_trials, np) .gt. rank) then
-      my_trials = my_trials+1
-   end if
-   call seed(rank)
+    r2 = r * r
+    call MPI_Bcast(num_trials, 1, MPI_INTEGER8, manager, MPI_COMM_WORLD, ierr)
 
-!$OMP parallel private(x,y) reduction(+:Ncirc)
-!$OMP do 
-     do i = 1, my_trials
-!$OMP critical (randoms)
-     x = lcgrandom()
-     y = lcgrandom()
-!$OMP end critical (randoms)
-     if ((x*x + y*y) .le. r2) then
-        Ncirc = Ncirc+1
-     end if
-     end do
-!$OMP end parallel
+    !$omp parallel
+    !$omp single
+    total_threads = omp_get_num_threads()
+    !$omp end single
+    !$omp end parallel
 
-     call MPI_Reduce(Ncirc, temp, 1, MPI_INTEGER, MPI_SUM, & 
-                     manager, MPI_COMM_WORLD, mpierr)
+    trials_per_rank = num_trials / int(size, 8)
+    trials_per_thread = trials_per_rank / int(total_threads, 8)
 
-     if (rank .eq. manager) then
-         Ncirc = temp
-         pi = 4.0*((1.0*Ncirc)/(1.0*num_trials))
-         print*, '     '
-         print*, '     Computing pi using hybrid MPI/OpenMP:'
-         print*, '     For ', num_trials, ' trials, pi = ', pi
-         print*, '     '
-      end if
-         call MPI_Finalize(mpierr)
+    !$omp parallel private(i, global_i, thread_seed, x, y, tid) reduction(+:local_rank_ncirc)
+    
+    tid = omp_get_thread_num()
+
+    do i = 0, trials_per_thread - 1
+        global_i = (int(rank, 8) * int(total_threads, 8)) + int(tid, 8) + &
+                   (i * (int(size, 8) * int(total_threads, 8)))
+        
+        thread_seed = base_seed + (global_i * 2_8)
+        
+        call lcgrandom_r(thread_seed, x)
+        call lcgrandom_r(thread_seed, y)
+        
+        if ((x*x + y*y) <= r2) then
+            local_rank_ncirc = local_rank_ncirc + 1_8
+        end if
+    end do
+    
+    !$omp end parallel
+
+    call MPI_Reduce(local_rank_ncirc, total_ncirc, 1, MPI_INTEGER8, MPI_SUM, manager, MPI_COMM_WORLD, ierr)
+
+    if (rank == manager) then
+        pi = 4.0_8 * real(total_ncirc, 8) / real(num_trials, 8)
+        print*, '     '
+        print*, '     Computing pi using hybrid MPI/OpenMP:'
+        print '(/,A,I0,A,F10.6,/)', '      For ', num_trials, ' trials, pi = ', pi
+        print*, '     '
+    end if
+
+    call MPI_Finalize(ierr)
 end program

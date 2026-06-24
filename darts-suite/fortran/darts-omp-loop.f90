2897a7307c866d93085847b6386669cfbf6daa22
helenhe40 <helenhe40@users.noreply.github.com>
2026-06-24 15:56:18 -0700

    a clean darts-omp-loop.f90 code

--- darts-suite/fortran/darts-omp-loop.f90
+++ darts-suite/fortran/darts-omp-loop.f90
@@ -1,6 +1,5 @@
 ! Compute pi using OpenMP Loop
 program darts
-    use iso_fortran_env, only: real64
     use lcgenerator
     use omp_lib
     implicit none
@@ -8,16 +7,16 @@ program darts
     integer(kind=8) :: num_trials = 1000000_8
     integer(kind=8) :: ncirc = 0_8
     integer(kind=8) :: i, thread_seed, base_seed = 12345_8
-    real(kind=8)    :: pi, x, y  ! Hoisted variables
+    real(kind=8)    :: pi, x, y
     real(kind=8)    :: r = 1.0_8
     real(kind=8)    :: r2
 
     r2 = r * r
 
-    !$omp parallel private(thread_seed, x, y) reduction(+:ncirc)
-    !$omp loop
-    do i = 1, num_trials
-        thread_seed = base_seed + ((i-1) * 2_8)
+    !$omp parallel loop private(thread_seed, x, y) reduction(+:ncirc)
+    do i = 0, num_trials - 1
+        ! Each iteration calculates its unique seed based on the loop index
+        thread_seed = base_seed + (i * 2_8)
         
         call lcgrandom_r(thread_seed, x)
         call lcgrandom_r(thread_seed, y)
@@ -26,12 +25,11 @@ program darts
             ncirc = ncirc + 1_8
         end if
     end do
-    !$omp end loop
-    !$omp end parallel
+    !$omp end parallel loop
 
     pi = 4.0_8 * real(ncirc, 8) / real(num_trials, 8)
     print*, '     '
-    print*, '     Computing pi using OpenMP Loop:'
+    print*, '     Computing pi using OpenMP:'
     print '(/,A,I0,A,F10.6,/)', '      For ', num_trials, ' trials, pi = ', pi
-    print*, '     '
-end program darts
+    print*, '     '     
+end program

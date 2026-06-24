e29d62ca8b5c68d1f7c1a78cb76eacb70c353fc8
helenhe40 <helenhe40@users.noreply.github.com>
2026-06-23 18:28:09 -0700

    update code using thread-safe random number generator

--- darts-suite/c/darts-mpi.c
+++ darts-suite/c/darts-mpi.c
@@ -3,46 +3,54 @@
 #include <mpi.h>
 #include <stdio.h>
 
-static long num_trials = 1000000;
-
 int main(int argc, char **argv) {
-  long i;
-  long Ncirc = 0;
-  double pi, x, y;
-  double r = 1.0; /* radius of circle */
-  double r2 = r*r;
 
-  int rank, size, manager = 0;
-  MPI_Status status;
-  long my_trials, temp;
-  int j;
+    long num_trials = 1000000;
+    long local_Ncirc = 0;
+    long total_Ncirc = 0;
+    long received_Ncirc;
+    double pi, x, y;
+    double r = 1.0;
+    double r2 = r*r;
+    long base_seed = 12345;
+    
+    int rank, size, manager = 0, source;
+    MPI_Status status;
 
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  my_trials = num_trials/size;
-  if (num_trials%(long)size > (long)rank) my_trials++;
-  random_last = rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  for (i = 0; i < my_trials; i++) {
-    x = lcgrandom();
-    y = lcgrandom();
-    if ((x*x + y*y) <= r2)
-      Ncirc++;
-  }
+    long my_trials = num_trials / size;
 
-  if (rank == manager) {
-    for (j = 1; j < size; j++) {
-      MPI_Recv(&temp, 1, MPI_LONG, j, j, MPI_COMM_WORLD, &status);
-      Ncirc += temp;
+    for (long i = 0; i < my_trials; i++) {
+        long thread_seed = base_seed + (rank + (i * size)) * 2L;
+
+        x = lcgrandom_r(&thread_seed);
+        y = lcgrandom_r(&thread_seed);
+
+        if ((x*x + y*y) <= r2) {
+            local_Ncirc++;
+        }
     }
-    pi = 4.0 * ((double)Ncirc)/((double)num_trials);
-    printf("\n \t Computing pi using six basic MPI functions: \n");
-    printf("\t For %ld trials, pi = %f\n", num_trials, pi);
-    printf("\n");
-  } else {
-    MPI_Send(&Ncirc, 1, MPI_LONG, manager, rank, MPI_COMM_WORLD);
-  }
-  MPI_Finalize();
-  return 0;
+
+    if (rank == manager) {
+        total_Ncirc = local_Ncirc;
+
+        for (source = 1; source < size; source++) {
+            MPI_Recv(&received_Ncirc, 1, MPI_LONG, source, 0, MPI_COMM_WORLD, &status);
+            total_Ncirc += received_Ncirc;
+        }
+
+        pi = 4.0 * ((double)total_Ncirc) / ((double)num_trials);
+        printf("\n \t Computing pi using six basic MPI functions: \n");
+        printf("\t For %ld trials, pi = %f\n", num_trials, pi);
+        printf("\n");
+    } 
+    else {
+        MPI_Send(&local_Ncirc, 1, MPI_LONG, manager, 0, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+    return 0;
 }

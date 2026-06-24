8f2e066a1e5c17f3b827d2e6cb34605aaee63e27
helenhe40 <helenhe40@users.noreply.github.com>
2026-06-23 18:35:08 -0700

    update code using thread-safe random number generator

--- darts-suite/fortran/darts-omp-loop.f90
+++ darts-suite/fortran/darts-omp-loop.f90
@@ -1,37 +1,37 @@
-! Compute pi using OpenMP "!$OMP loop"
-
-! Written by Helen He, June 2024
-! In this code, we are not using the pseudorandom number generator used in other codes,
-! instead we use a complete random number generator from Fortran intrinsic randon_number()
-! so we do not need to use an OMP critial region (which is not allowed to nest under omp loop)
-! to reproduce the randon seeds.  
-! so the final pi value is not bit-for-bit from other codes
-! Also, it gives a different pi value every time it runs
-
-! Now, we compute pi
+! Compute pi using OpenMP Loop
 program darts
-   implicit none
-   integer*8 :: num_trials = 1000000, i = 0, Ncirc = 0
-   real :: pi = 0.0, x = 0.0, y = 0.0, r = 1.0
-   real :: r2 = 0.0
-   r2 = r*r
+    use iso_fortran_env, only: real64
+    use lcgenerator
+    use omp_lib
+    implicit none
+    
+    integer(kind=8) :: num_trials = 1000000_8
+    integer(kind=8) :: ncirc = 0_8
+    integer(kind=8) :: i, thread_seed, base_seed = 12345_8
+    real(kind=8)    :: pi, x, y  ! Hoisted variables
+    real(kind=8)    :: r = 1.0_8
+    real(kind=8)    :: r2
 
-!$OMP parallel private(x,y) reduction(+:Ncirc)
-!$OMP loop
+    r2 = r * r
+
+    !$omp parallel private(thread_seed, x, y) reduction(+:ncirc)
+    !$omp loop
     do i = 1, num_trials
-       call random_number(x)
-       call random_number(y)
-       if ((x*x + y*y) .le. r2) then
-          Ncirc = Ncirc+1
-       end if
+        thread_seed = base_seed + ((i-1) * 2_8)
+        
+        call lcgrandom_r(thread_seed, x)
+        call lcgrandom_r(thread_seed, y)
+        
+        if ((x*x + y*y) <= r2) then
+            ncirc = ncirc + 1_8
+        end if
     end do
-!$OMP end loop
-!$OMP end parallel
+    !$omp end loop
+    !$omp end parallel
 
-    pi = 4.0*((1.0*Ncirc)/(1.0*num_trials))
+    pi = 4.0_8 * real(ncirc, 8) / real(num_trials, 8)
     print*, '     '
-    print*, '     Computing pi using OpenMP:         '
-    print*, '     For ', num_trials, ' trials, pi = ', pi
+    print*, '     Computing pi using OpenMP Loop:'
+    print '(/,A,I0,A,F10.6,/)', '      For ', num_trials, ' trials, pi = ', pi
     print*, '     '
-
-end program
+end program darts

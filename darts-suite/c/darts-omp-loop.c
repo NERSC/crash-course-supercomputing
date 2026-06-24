9bcb1492e8fda2ba245991c5713bd098bdd805db
helenhe40 <helenhe40@users.noreply.github.com>
2026-06-23 18:27:28 -0700

    update code using thread-safe random number generator

--- darts-suite/c/darts-omp-loop.c
+++ darts-suite/c/darts-omp-loop.c
@@ -1,43 +1,36 @@
-/* Compute pi using OpenMP "pragma omp loop" */ 
-
-/*  Written by Helen He, June 2024
-    In this code, we are not using the pseudorandom number generator used in other codes,
-    instead we use a complete random number generator rand() in C
-    so we do not need to use an OMP critial region (which is not allowed to nest under omp loop)
-    to reproduce the randon seeds.  
-    so the final pi value is not bit-for-bit from other codes
-    Also, it gives a different pi value every time it runs
-*/
-
+/* Compute pi using OpenMP Loop */ 
 #include "lcgenerator.h"
 #include <omp.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-static long num_trials = 1000000; 
+int main() {
+    long num_trials = 1000000;
+    long Ncirc = 0;
+    double pi;
+    double r = 1.0;
+    double r2 = r*r;
+    long base_seed = 12345;
+    long i;
 
-int main(int argc, char **argv) {
-  long i;
-  long Ncirc = 0;
-  double pi, x, y;
-  double r = 1.0; /* radius of circle */
-  double r2 = r*r;
-    
-#pragma omp parallel private(x,y) reduction(+:Ncirc)
-{
-#pragma omp loop
-  for (i = 0; i < num_trials; i++) {
-     x = (double) rand() / RAND_MAX;
-     y = (double) rand() / RAND_MAX;
-     if ((x*x + y*y) <= r2)
-        Ncirc++;
-  }
-}
+#pragma omp parallel reduction(+:Ncirc)
+    {
+        double x, y;
+    #pragma omp loop
+        for (i = 0; i < num_trials; i++) {
+            long thread_seed = base_seed + (i * 2L);
 
-  pi = 4.0 * ((double)Ncirc)/((double)num_trials);
-  printf("\n \t Computing pi using OpenMP: \n");
-  printf("\t For %ld trials, pi = %f\n", num_trials, pi);
-  printf("\n");
+            x = lcgrandom_r(&thread_seed);
+            y = lcgrandom_r(&thread_seed);
 
-  return 0;
+            if ((x*x + y*y) <= r2) {
+                Ncirc++;
+            }
+        }
+    }
+
+    pi = 4.0 * ((double)Ncirc) / ((double)num_trials);
+    printf("\n \t Computing pi using OpenMP: \n");
+    printf("\t For %ld trials, pi = %f\n", num_trials, pi);
+    printf("\n");
+    return 0;
 }

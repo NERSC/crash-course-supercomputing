9753ae1448c098b3e0201cc9f0f02896711478e5
helenhe40 <helenhe40@users.noreply.github.com>
2026-06-23 18:30:33 -0700

    update code using thread-safe random number generator

--- darts-suite/c/darts-hybrid.c
+++ darts-suite/c/darts-hybrid.c
@@ -4,51 +4,60 @@
 #include <omp.h>
 #include <stdio.h>
 
-static long num_trials = 1000000;
-
 int main(int argc, char **argv) {
-  long i;
-  long Ncirc = 0;
-  double pi, x, y;
-  double r = 1.0; /* radius of circle */
-  double r2 = r*r;
 
-  int rank, size, manager = 0;
-  MPI_Status status;
-  long my_trials, temp;
-  int provided;
+    long num_trials = 1000000;  /* Statically initialized on all processes */
+    long total_Ncirc = 0;
+    long local_rank_Ncirc = 0;
+    double pi;
+    double r = 1.0;
+    double r2 = r*r;
+    long base_seed = 12345;
 
-  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  my_trials = num_trials/size;
-  if (num_trials%(long)size > (long)rank) my_trials++;
-  random_last = rank;
+    int rank, size, manager = 0;
+    int total_threads = 1;
+    int provided;
 
-#pragma omp parallel
-{
-#pragma omp for private(x,y) reduction(+:Ncirc)
-  for (i = 0; i < my_trials; i++) {
-#pragma omp critical (randoms)
-{
-    x = lcgrandom();
-    y = lcgrandom();
-}
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if ((x*x + y*y) <= r2)
-      Ncirc++;
-  }
-}
+    #pragma omp parallel
+    {
+        #pragma omp single
+        total_threads = omp_get_num_threads();
+    }
 
-  MPI_Reduce(&Ncirc, &temp, 1, MPI_LONG, MPI_SUM, manager, MPI_COMM_WORLD);
+    long trials_per_rank = num_trials / size;
+    long trials_per_thread = trials_per_rank / total_threads;
 
-  if (rank == manager) {
-    Ncirc = temp;
-    pi = 4.0 * ((double)Ncirc)/((double)num_trials);
-    printf("\n \t Computing pi using hybrid MPI/OpenMP: \n");
-    printf("\t For %ld trials, pi = %f\n", num_trials, pi);
-    printf("\n");
-  } 
-  MPI_Finalize();
-  return 0;
+    #pragma omp parallel reduction(+:local_rank_Ncirc)
+    {
+        int tid = omp_get_thread_num();
+        double x, y;
+
+        for (long i = 0; i < trials_per_thread; i++) {
+            long global_i = (rank * total_threads) + tid + (i * (size * total_threads));
+            long thread_seed = base_seed + (global_i * 2L);
+
+            x = lcgrandom_r(&thread_seed);
+            y = lcgrandom_r(&thread_seed);
+
+            if ((x*x + y*y) <= r2) {
+                local_rank_Ncirc++;
+            }
+        }
+    }
+
+    MPI_Reduce(&local_rank_Ncirc, &total_Ncirc, 1, MPI_LONG, MPI_SUM, manager, MPI_COMM_WORLD);
+
+    if (rank == manager) {
+        pi = 4.0 * ((double)total_Ncirc) / ((double)num_trials);
+        printf("\n \t Computing pi using hybrid MPI/OpenMP: \n");
+        printf("\t For %ld trials, pi = %f\n", num_trials, pi);
+        printf("\n");
+    }
+
+    MPI_Finalize();
+    return 0;
 }

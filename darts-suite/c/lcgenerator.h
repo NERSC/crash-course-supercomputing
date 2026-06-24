ca24f5b349ed8ceaeab023d1f2e21529008eca02
helenhe40 <helenhe40@users.noreply.github.com>
2026-06-23 18:24:26 -0700

    thread-safe lcgenerator
    Updated comments to reflect thread-safety and modified the random number generator to be thread-safe.

--- darts-suite/c/lcgenerator.h
+++ darts-suite/c/lcgenerator.h
@@ -1,16 +1,13 @@
-/* Random number generator -- and not a very good one, either! */
-
+/* Thread-safe random number generator -- and not a very good one, either! */
 static long MULTIPLIER = 1366;
 static long ADDEND = 150889;
 static long PMOD = 714025;
-long random_last = 0;
 
-/* This is not a thread-safe random number generator */
+/* Thread-safe (reentrant) version of the linear congruential generator */
+double lcgrandom_r(long *random_last) { 
+    long random_next; 
+    random_next = (MULTIPLIER * (*random_last) + ADDEND) % PMOD; 
+    *random_last = random_next;
 
-double lcgrandom() {
-  long random_next;
-  random_next = (MULTIPLIER * random_last + ADDEND)%PMOD;
-  random_last = random_next;
-
-  return ((double)random_next/(double)PMOD);
+    return ((double)random_next / (double)PMOD);
 }

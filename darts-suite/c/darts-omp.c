c45ee61de48f747b33099b1c768c6eeab3b02026
helenhe40 <helenhe40@users.noreply.github.com>
2026-06-23 18:26:42 -0700

    update code using thread-safe random number generator

--- darts-suite/c/darts-omp.c
+++ darts-suite/c/darts-omp.c
@@ -3,32 +3,38 @@
 #include <omp.h>
 #include <stdio.h>
 
-static long num_trials = 1000000;
+int main() {
+    long num_trials = 1000000;
+    long Ncirc = 0;
+    double pi;
+    double r = 1.0;
+    double r2 = r*r;
+    long base_seed = 12345;
 
-int main(int argc, char **argv) {
-  long i;
-  long Ncirc = 0;
-  double pi, x, y;
-  double r = 1.0; /* radius of circle */
-  double r2 = r*r;
-#pragma omp parallel
-{
-#pragma omp for private(x,y) reduction(+:Ncirc)
-  for (i = 0; i < num_trials; i++) {
-#pragma omp critical (randoms)
-{
-    x = lcgrandom();
-    y = lcgrandom();
-}
-    if ((x*x + y*y) <= r2)
-      Ncirc++;
-  }
-}
+#pragma omp parallel reduction(+:Ncirc)
+    {
+        long i;
+        double x, y;
+        int tid = omp_get_thread_num();
+        int total_threads = omp_get_num_threads();
+        long my_trials = num_trials / total_threads;
 
-  pi = 4.0 * ((double)Ncirc)/((double)num_trials);
-  printf("\n \t Computing pi using OpenMP: \n");
-  printf("\t For %ld trials, pi = %f\n", num_trials, pi);
-  printf("\n");
+        for (i = 0; i < my_trials; i++) {
+            long global_i = tid + (i * total_threads);
+            long thread_seed = base_seed + (global_i * 2L);
 
-  return 0;
+            x = lcgrandom_r(&thread_seed);
+            y = lcgrandom_r(&thread_seed);
+
+            if ((x*x + y*y) <= r2) {
+                Ncirc++;
+            }
+        }
+    }
+
+    pi = 4.0 * ((double)Ncirc) / ((double)num_trials);
+    printf("\n \t Computing pi using OpenMP: \n");
+    printf("\t For %ld trials, pi = %f\n", num_trials, pi);
+    printf("\n");
+    return 0;
 }
